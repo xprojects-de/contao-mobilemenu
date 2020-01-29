bea295fd76c5a0a38a92d8ee04effc076dfd47e1
XProjects <infos@x-projects.de>
2020-01-29 14:50:54 +0100

    Move HTML to Template

--- contao-mobilemenu/src/Classes/ModuleXMobileMenue.php
+++ contao-mobilemenu/src/Classes/ModuleXMobileMenue.php
@@ -12,9 +12,6 @@ class ModuleXMobileMenue extends \Module {
     $GLOBALS['TL_JAVASCRIPT'][] = $assetsDir . '/js/nav_jquery.js|static';
     //$GLOBALS['TL_CSS'][] = $assetsDir . '/scss/nav_jquery.css|static';
 
-    $imageNormal = $this->xgetImage($this->xmobilemenuemicon);
-    $imageClose = $this->xgetImage($this->xmobilemenuemcloseicon);
-
     $js_query = '<script src="system/modules/xmobilemenu/assets/nav_jquery.js"></script>
         <script>
             (function ($) {
@@ -35,19 +32,24 @@ class ModuleXMobileMenue extends \Module {
 
     $GLOBALS['TL_HEAD'][] = $css;
     $GLOBALS['TL_JQUERY'][] = $js_query;
+
+    $imageNormal = $this->xgetImage($this->xmobilemenuemicon);
     $icon = "";
     if ($imageNormal != "") {
       $icon = '<img src="' . $imageNormal . '" />';
     }
-    $closeArea = '';
-    if ($this->xmobilemenuemcompletesize == 1) {
-      $closeArea = '<div class="xmobilemenue_maincontainer_inside_close"><a href="#" onclick="return false;"><img src="' . $imageClose . '" /></a></div><div style="clear:both;"></div>';
+
+    $imageClose = $this->xgetImage($this->xmobilemenuemcloseicon);
+    $iconClose = "";
+    if ($imageClose != "") {
+      $iconClose = '<img src="' . $imageClose . '" />';
     }
-    $returnvalue = '<div class="xmobilemenue" id="xmobilemenue_' . $this->id . '">
-            <div class="xmobilemenue_btcontainer" id="xmobilemenue_btcontainer_' . $this->id . '"><span id="xmobilemenuebt_' . $this->id . '">' . $icon . '</span></div>
-            <div class="xmobilemenue_maincontainer" id="xmobilemenue_maincontainer_' . $this->id . '"><div class="xmobilemenue_maincontainer_inside">' . $closeArea . $this->replaceInsertTags('{{insert_module::' . $this->xmobilemenuemid . '}}') . '</div></div>    
-            </div>';
-    $this->Template->xausgabe = $returnvalue;
+
+    $this->Template->id = $this->id;
+    $this->Template->closearea = ($this->xmobilemenuemcompletesize == 1);
+    $this->Template->modulePlaceholder = $this->replaceInsertTags('{{insert_module::' . $this->xmobilemenuemid . '}}');
+    $this->Template->icon = $icon;
+    $this->Template->iconClose = $iconClose;
   }
 
   private function xgetImage($uuid) {
