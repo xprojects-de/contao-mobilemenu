49e767af609fdfa1403eaaf45cef5deaba790168
Benjamin Hummel <55982921+xprojects-de@users.noreply.github.com>
2021-10-29 12:13:38 +0200

    Bugfixes

--- contao-mobilemenu/src/Resources/public/js/nav_jquery.js
+++ contao-mobilemenu/src/Resources/public/js/nav_jquery.js
@@ -13,29 +13,44 @@
 
   $.XMobileMenu.prototype = {
     init: function () {
+
       var self = this;
       this.setModeSettings();
-      this.buttonelement.css({cursor: 'pointer'});
+
+      this.buttonelement.css({ cursor: 'pointer' });
+
       this.setPosition();
       this.setLinksActivation();
+
       this.buttonelement.click(function () {
+
         if (self.containerelement.css("display") === 'none') {
+
           self.containerelement.fadeIn();
           self.containerelement.width($(window).width() + "px");
           self.buttonelement.addClass('xmobilemenuactive');
+
           if (self.iconclose !== '' && self.completeSize === false) {
+
             self.buttonelement.find('img').each(function () {
               $(this).attr('src', self.iconclose);
             });
+
           }
+
         } else {
+
           self.containerelement.fadeOut(750);
           self.buttonelement.removeClass('xmobilemenuactive');
+
           if (self.icon !== '' && self.completeSize === false) {
+
             self.buttonelement.find('img').each(function () {
               $(this).attr('src', self.icon);
             });
+
           }
+
         }
       });
       $(window).on('resize', function () {
@@ -43,27 +58,40 @@
       });
     },
     setModeSettings: function () {
-      this.containerelement.css({width: '100%', 'z-index': 50000, margin: 0, left: 0, top: 0, position: 'absolute'});
+      this.containerelement.css({ width: '100%', overflow: 'auto', 'z-index': 50000, margin: 0, left: 0, top: 0, position: 'absolute' });
     },
     setPosition: function () {
+
+      var maxHeight = $(window).height() - (this.buttonelement.position().top + this.buttonelement.height());
+
       if (this.completeSize === true) {
-        this.containerelement.css({height: $(window).height()});
+        this.containerelement.css({ height: $(window).height() });
       } else {
-        this.containerelement.css({top: (this.buttonelement.position().top + this.buttonelement.height())});
+        this.containerelement.css({ top: (this.buttonelement.position().top + this.buttonelement.height()), 'max-height': maxHeight + 'px', width: '100%' });
       }
+
     },
     setLinksActivation: function () {
+
       var self = this;
+
       $(this.containerelement).find('a').each(function () {
+
         $(this).click(function () {
+
           self.containerelement.fadeOut(750);
           if (self.icon !== '' && self.completeSize === false) {
+
             self.buttonelement.find('img').each(function () {
               $(this).attr('src', self.icon);
             });
+
           }
+
         });
+
       });
+
     }
   };
 
